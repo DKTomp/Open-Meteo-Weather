3de9f88ea787191774df1cec9ebe5f00ab742ec8
DKTomp <dylan.tompkins1991@yahoo.com>
2026-01-08 01:25:51 -0600

    fixed validation error

--- js/main.js
+++ js/main.js
@@ -16,9 +16,13 @@ const pressE = [
     "550hPa"
 ]
 
-
-
 let hour = 1
+let upperDataVar;
+let weatherData;
+let upperSpeed;
+let upperTemp;
+let forecastSpeed;
+let forecastTemp;
 
 const prevButton = document.getElementById("prevHour")
 const nextButton = document.getElementById("nextHour")
@@ -40,34 +44,33 @@ async function generalData() {
         let weatherResponse = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=33.45&longitude=-96.38&current=temperature_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m,cloud_cover,apparent_temperature,relative_humidity_2m,&current=is_day&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`)
         weatherData = await weatherResponse.json()
         currentSurfaceWeather()
-        console.log(weatherData)
-        //console.log(weatherData.current)
+        
         } catch (error) {
         console.log(error)
     }
 }
 
 function currentSurfaceWeather() {
-        let isDay = weatherData.current.is_day
+    let isDay = weatherData.current.is_day
 
-        let clock = getTime()
-        document.getElementById("time").innerText = clock
+    let clock = getTime()
+    document.getElementById("time").innerText = clock
 
-        let curTemp = Math.round(weatherData.current.temperature_2m)
-        document.getElementById("cur-temp").innerText = `${curTemp}°F`
-        let feelTemp = Math.round(weatherData.current.apparent_temperature)
-        document.getElementById("feels-temp").innerText = `Feels Like: ${feelTemp}°F`
-        let surWind = Math.round(weatherData.current.wind_speed_10m)
-        let windDir = determineWindDir(weatherData.current.wind_direction_10m)
-        document.getElementById("sur-wind").innerText = `${windDir} ${surWind} mph`
-        let windGust = Math.round(weatherData.current.wind_gusts_10m)
-        document.getElementById("wind-gust").innerText = `${windGust} mph`
-        let cloudCvr = Math.round(weatherData.current.cloud_cover)
-        document.getElementById("cloud-cvr").innerText = `${cloudCvr}%`
-        let humidity = Math.round(weatherData.current.relative_humidity_2m)
-        document.getElementById("humidity").innerText = `${humidity}%`
+    let curTemp = Math.round(weatherData.current.temperature_2m)
+    document.getElementById("cur-temp").innerText = `${curTemp}°F`
+    let feelTemp = Math.round(weatherData.current.apparent_temperature)
+    document.getElementById("feels-temp").innerText = `Feels Like: ${feelTemp}°F`
+    let surWind = Math.round(weatherData.current.wind_speed_10m)
+    let windDir = determineWindDir(weatherData.current.wind_direction_10m)
+    document.getElementById("sur-wind").innerText = `${windDir} ${surWind} mph`
+    let windGust = Math.round(weatherData.current.wind_gusts_10m)
+    document.getElementById("wind-gust").innerText = `${windGust} mph`
+    let cloudCvr = Math.round(weatherData.current.cloud_cover)
+    document.getElementById("cloud-cvr").innerText = `${cloudCvr}%`
+    let humidity = Math.round(weatherData.current.relative_humidity_2m)
+    document.getElementById("humidity").innerText = `${humidity}%`
 
-        svgIcon(cloudCvr, isDay)   
+    svgIcon(cloudCvr, isDay)   
 }
 
 function calculateC() {
@@ -83,7 +86,7 @@ function calculateC() {
         feelTemp = Math.round((feelTemp - 32) * (5 / 9))
         document.getElementById("feels-temp").innerText = `Feels Like: ${feelTemp}°C`
 
-        for (i = 0; i < upperTemp.length; i++) {
+        for (let i = 0; i < upperTemp.length; i++) {
             let upperTempC = Math.round((upperTemp[i] - 32) * (5 / 9))
             let forUpperTempC = Math.round((forecastTemp[i] - 32) * (5 / 9))
             document.getElementById('temp' + i).innerText = `${upperTempC}°C`
@@ -103,7 +106,7 @@ function CalculateF() {
         let feelTemp = Math.round(weatherData.current.apparent_temperature)
         document.getElementById("feels-temp").innerText = `Feels Like: ${feelTemp}°C`
 
-        for (i = 0; i < upperTemp.length; i++) {
+        for (let i = 0; i < upperTemp.length; i++) {
             document.getElementById('temp' + i).innerText = `${upperTemp[i]}°F`
             document.getElementById('fortemp' + i).innerText = `${forecastTemp[i]}°F`
         }
@@ -120,7 +123,6 @@ function calculateKPH() {
             ktsButton.classList.toggle('selected')
         }
         
-
         let surWind = weatherData.current.wind_speed_10m
         let windDir = determineWindDir(weatherData.current.wind_direction_10m)
         surWind = Math.round(surWind * 1.609)
@@ -129,7 +131,7 @@ function calculateKPH() {
         windGust = Math.round(windGust * 1.609)
         document.getElementById("wind-gust").innerText = `${windGust} km/h`
         
-        for (i = 0; i <  15; i++) {
+        for ( let i = 0; i <  15; i++) {
             let calcKPH = Math.round(upperSpeed[i] * 1.609)
             let calcforKPH = Math.round(forecastSpeed[i] * 1.151)
             document.getElementById('speed' + i).innerText = `at ${calcKPH} km/h`
@@ -155,10 +157,10 @@ function calcuateMPH() {
     let windGust = Math.round(weatherData.current.wind_gusts_10m)
     document.getElementById("wind-gust").innerText = `${windGust} mph`
 
-    for (i = 0; i <  15; i++) {
-            document.getElementById('speed' + i).innerText = `at ${upperSpeed[i]} mph`
-            document.getElementById('forspeed' + i).innerText = `at ${forecastSpeed[i]} mph`
-        }
+    for (let i = 0; i <  15; i++) {
+        document.getElementById('speed' + i).innerText = `at ${upperSpeed[i]} mph`
+        document.getElementById('forspeed' + i).innerText = `at ${forecastSpeed[i]} mph`
+    }
 }
 
 function calculateKTS() {
@@ -180,12 +182,12 @@ function calculateKTS() {
     windGust = Math.round(windGust * 1.151)
     document.getElementById("wind-gust").innerText = `${windGust} kts`
 
-    for (i = 0; i <  15; i++) {
-            let calcKTS = Math.round(upperSpeed[i] * 1.151)
-            let calcforKTS = Math.round(forecastSpeed[i] * 1.151)
-            document.getElementById('speed' + i).innerText = `at ${calcKTS} kts`
-            document.getElementById('forspeed' + i).innerText = `at ${calcforKTS} kts`
-        }
+    for (let i = 0; i <  15; i++) {
+        let calcKTS = Math.round(upperSpeed[i] * 1.151)
+        let calcforKTS = Math.round(forecastSpeed[i] * 1.151)
+        document.getElementById('speed' + i).innerText = `at ${calcKTS} kts`
+        document.getElementById('forspeed' + i).innerText = `at ${calcforKTS} kts`
+    }
 }
 
 function svgIcon(cvr, day) {
@@ -240,7 +242,7 @@ function determineWindDir(dir) {
         dir = dir - 360
     }
 
-    for (i = 0; i <= 15; i++){
+    for (let i = 0; i <= 15; i++){
         if (dir < 10.5) {
             cardDir = directions[i].val
             return cardDir
@@ -256,25 +258,21 @@ function determineWindDir(dir) {
 async function uppersData() {
     try {
         let upperResponse = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=33.45&longitude=-96.38&hourly=temperature_2m,temperature_950hPa,temperature_925hPa,temperature_875hPa,temperature_850hPa,temperature_825hPa,temperature_775hPa,temperature_750hPa,temperature_725hPa,temperature_700hPa,temperature_675hPa,temperature_650hPa,temperature_625hPa,temperature_600hPa,temperature_575hPa,temperature_550hPa,wind_speed_950hPa,wind_speed_925hPa,wind_speed_875hPa,wind_speed_850hPa,wind_speed_825hPa,wind_speed_775hPa,wind_speed_750hPa,wind_speed_725hPa,wind_speed_700hPa,wind_speed_675hPa,wind_speed_650hPa,wind_speed_625hPa,wind_speed_600hPa,wind_speed_575hPa,wind_speed_550hPa,wind_direction_950hPa,wind_direction_925hPa,wind_direction_875hPa,wind_direction_850hPa,wind_direction_825hPa,wind_direction_775hPa,wind_direction_750hPa,wind_direction_725hPa,wind_direction_700hPa,wind_direction_675hPa,wind_direction_650hPa,wind_direction_625hPa,wind_direction_600hPa,wind_direction_575hPa,wind_direction_550hPa&models=gfs_seamless&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit`)
-        globalThis.upperDataVar = await upperResponse.json()
+        upperDataVar = await upperResponse.json()
         parseCurrentData(pressE)
         ParseForecastData(pressE)
         
-        
-
         } catch (error) {
             console.log(error)
     }
 }
 
 function parseCurrentData(pressElevation) {
-    
-    //console.log(uppers)
-    globalThis.upperSpeed = []
+    upperSpeed = []
     let upperDir = []
-    globalThis.upperTemp = []
+    upperTemp = []
 
-    for (e of pressElevation) {
+    for (let e of pressElevation) {
         const speedConcact = "wind_speed_" + e
         const dirConcact = "wind_direction_" + e
         const tempConcact = "temperature_" + e
@@ -288,7 +286,7 @@ function parseCurrentData(pressElevation) {
         upperTemp.push(upTemp)
     }
     
-    for (i = 0; i < 15; i++) {
+    for (let i = 0; i < 15; i++) {
         document.getElementById('dir' + i).innerText = `${upperDir[i]}°`
         document.getElementById('speed' + i).innerText = `at ${upperSpeed[i]} mph`
         document.getElementById('temp' + i).innerText = `${upperTemp[i]}°F`
@@ -296,17 +294,15 @@ function parseCurrentData(pressElevation) {
 }
 
 function ParseForecastData(pressElevation) {
-    clock = getForecastTime()
+    let clock = getForecastTime()
     let forecastdisplay = `Forecast Time: ${clock}`
-    
     document.getElementById("forecastHeader").innerText = forecastdisplay
 
-    globalThis.forecastSpeed = []
+    forecastSpeed = []
     let forecastDir = []
-    globalThis.forecastTemp = []
+    forecastTemp = []
 
-
-    for (e of pressElevation) {
+    for (let e of pressElevation) {
         const speedConcact1 = "wind_speed_" + e
         const dirConcact1 = "wind_direction_" + e
         const tempConcact1 = "temperature_" + e
@@ -321,30 +317,30 @@ function ParseForecastData(pressElevation) {
     }
 
     if (mphButton.classList.contains('selected')) {
-        for (i = 0; i < 15; i++) {
+        for (let i = 0; i < 15; i++) {
             document.getElementById('fordir' + i).innerText = `${forecastDir[i]}°`
             document.getElementById('forspeed' + i).innerText = `at ${forecastSpeed[i]} mph`
         }
     } else if (kphButton.classList.contains('selected')) {
-        for (i = 0; i < 15; i++) {
-            forecastkphSpeed = Math.round(forecastSpeed[i] * 1.609)
+        for (let i = 0; i < 15; i++) {
+            let forecastkphSpeed = Math.round(forecastSpeed[i] * 1.609)
             document.getElementById('fordir' + i).innerText = `${forecastDir[i]}°`
             document.getElementById('forspeed' + i).innerText = `at ${forecastkphSpeed} km/h`
         }
     } else if (ktsButton.classList.contains('selected')) {
-        for (i = 0; i < 15; i++) {
-            forecastktsSpeed = Math.round(forecastSpeed[i] * 1.151)
+        for (let i = 0; i < 15; i++) {
+            let forecastktsSpeed = Math.round(forecastSpeed[i] * 1.151)
             document.getElementById('fordir' + i).innerText = `${forecastDir[i]}°`
             document.getElementById('forspeed' + i).innerText = `at ${forecastktsSpeed} kts`
         }
     }
 
     if (farButton.classList.contains('selected')) {
-        for (i = 0; i < 15; i++) {
+        for (let i = 0; i < 15; i++) {
             document.getElementById('fortemp' + i).innerText = `${forecastTemp[i]}°F`
         }
     } else if (celButton.classList.contains('selected')) {
-        for (i = 0; i < 15; i++) {
+        for (let i = 0; i < 15; i++) {
             let celtemp = Math.round((forecastTemp[i] - 32) * (5 / 9))
             document.getElementById('fortemp' + i).innerText = `${celtemp}°C`
         }
@@ -410,6 +406,3 @@ function nextHour() {
 generalData()
 uppersData()
 getTime()
-
-
-
